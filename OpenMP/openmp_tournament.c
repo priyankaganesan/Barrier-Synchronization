e5554418c66b570d33669f22486b4dee453b55f0
Priyanka Ganesan <priyanka.ganesan@gatech.edu>
2015-10-12 15:17:18 -0400

    Coded OMP tournament

--- OpenMP/openmp_tournament.c
+++ OpenMP/openmp_tournament.c
@@ -13,13 +13,12 @@
 
 typedef struct record{
     int role;
-    int opponent;
+    int *opponent;
     int flag;
 }record_t;
 
 record_t** players;
 int P, N;
-bool global_sense = true;
 
 void barrier_init()
 {
@@ -35,38 +34,76 @@ void barrier_init()
 		for(k=0;k<=rounds;k++)
 		{
 			//Initializing flag
-			players[i][k].flag = false;
+			players[i][k].flag = 0;
 			
 			//Initializing role
 			if (k==0){
 				players[i][k].role = DROPOUT;
 			}
 			else if (k>0){
-				if (i == 0 && (1<<k)>=P)
+				if (i == 0 && (1<<k)>=P){
 					players[i][k].role = CHAMPION;
+					players[i][k].opponent = &players[i+(1<<(k-1))][k].flag;
+				}
 				else if (i%(1<<k) == 0){
 					if (((i + (1<<(k-1))) < P) && ((1<<k) < P)){
                     				players[i][k].role = WINNER;
+						players[i][k].opponent = &players[i+(1<<(k-1))][k].flag;
 					} else if ((i + (1<<(k-1))) >= P){
 						players[i][k].role = BYE;
 					}
 				}
 				else if ((i%(1<<k)) == (1<<(k-1))){
 					players[i][k].role = LOSER;
+					players[i][k].opponent = &players[i-(1<<(k-1))][k].flag;
 				}
 			}
-
-			//Initializing opponent
-			players[i][k].opponent = false;
 		}
 	}		
 }
 
-void tournament_barrier(bool *pid_sense)
+void tournament_barrier(int *pid_sense)
 {
 	int i = 1;
-	
-	global_sense = !global_sense;	
+	int vpid = omp_get_thread_num();
+	for(i=1; i<=log(P)/log(2); i++)
+	{
+		switch (players[vpid][i].role)
+		{
+			case WINNER:
+				while (players[vpid][i].flag == *pid_sense);
+				break;
+			case LOSER:
+				players[vpid][i].opponent = pid_sense;
+				break;
+			case BYE: 
+				break;
+			case CHAMPION:
+				while (players[vpid][i].flag == *pid_sense);
+				players[vpid][i].opponent = pid_sense;
+				break;
+			case DROPOUT:
+				break;
+		}
+	}
+	for(i=3; i>0; i--)
+	{
+		switch (players[vpid][i].role)
+		{
+			case WINNER:
+				players[vpid][i].opponent = pid_sense;
+				break;
+			case LOSER:
+				break;
+			case BYE:
+				break;
+			case CHAMPION:
+				break;
+			case DROPOUT:
+				break;
+		}
+	}
+	*pid_sense = !*pid_sense;	
 }
 
 int main(int argc, char* argv[])
@@ -75,9 +112,9 @@ int main(int argc, char* argv[])
 	P = 8;
 	//Number of loops
 	N = 4;
-	bool pid_sense = 1;
+	int pid_sense = 1;
 	barrier_init();
-	#pragma omp parallel num_threads(P) shared() firstprivate(pid_sense, N)
+	#pragma omp parallel num_threads(P) firstprivate(pid_sense, N)
 	{
 		int i;
 		for(i=0;i<N;i++)
