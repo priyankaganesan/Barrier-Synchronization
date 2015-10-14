9271e5d02a34466beabc3dd906bee0583fc4fbc1
Priyanka Ganesan <priyanka.ganesan@gatech.edu>
2015-10-14 16:24:11 -0400

    tournamentmpi updated

--- MPI/tournamentmpi.c
+++ MPI/tournamentmpi.c
@@ -31,7 +31,7 @@ void tournament_barrier_init()
   int i, k;
   MPI_Comm_size(MPI_COMM_WORLD,&P);
   MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-  int rounds = (int)(log(P)/log(2));
+  int rounds = ceil((log(P)/log(2)));
 //  printf("Rounds is %d\n", rounds);
   record = (record_t*) calloc(rounds+1,sizeof(record_t));
   int power_of_round;
@@ -85,7 +85,7 @@ void tournament_barrier()
   while(flag)
   {
     round=round+1;
-    printf("Processor %d reached barrier in round %d\n", rank,round);
+    //printf("Processor %d reached barrier in round %d\n", rank,round);
     switch(record[round].role)
     {
       case WINNER:
@@ -127,7 +127,7 @@ void tournament_barrier()
         break;        
     }
   }
-  printf("Processor %d finished barrier\n",rank);
+  //printf("Processor %d finished barrier\n",rank);
 }
 
 void tournament_barrier_finish()
@@ -149,40 +149,41 @@ int main(int argc, char **argv)
           printf ("N - not an integer\n");
   }
   else
-    N = 1000;
+    N = 5;
   
   struct timeval tv1, tv2;
   double total_time;
-  // MPI_Init(&argc, &argv);
 
   tournament_barrier_init();
-  //TODO: Need to check process id?
-  gettimeofday(&tv1, NULL);
+  if (rank == 0 ){
+	gettimeofday(&tv1, NULL);
+  }
   for(k=0;k<N;k++)
   {
+	printf ("================LOOP %d ===========================\n",k);
+	printf ("Before barrier 1\n");
     tournament_barrier();
-    for(i=0;i<10000;i++)
-      for(j=0;j<10000;j++);
+	printf ("Before barrier 2\n");
     tournament_barrier();
-    for(i=0;i<10000;i++)
-      for(j=0;j<10000;j++);
+	printf ("Before barrier 3\n");
     tournament_barrier();
-    for(i=0;i<10000;i++)
-      for(j=0;j<10000;j++);
+	printf ("Before barrier 4\n");
     tournament_barrier();
-    for(i=0;i<10000;i++)
-      for(j=0;j<10000;j++);
+	printf ("Before barrier 5\n");
     tournament_barrier();
   }
-  gettimeofday(&tv2, NULL);
-
-  total_time = (double) (tv2.tv_usec - tv1.tv_usec) + (double) (tv2.tv_sec - tv1.tv_sec)*1000000;
-    printf("\nSUMMARY:\nNumber of processes: %d\n Total run-time for %d "
-            "loops with 5 barriers per loop: %fs\n"
-            "The average time per barrier: %fus\n",
-            P, N, total_time/1000000, (double)(total_time/(N*5)));
+  if (rank == 0){
+	gettimeofday(&tv2, NULL);
+  }
   
  tournament_barrier_finish();
   MPI_Finalize();
+  if (rank == 0){
+	total_time = (double) (tv2.tv_usec - tv1.tv_usec) + (double) (tv2.tv_sec - tv1.tv_sec)*1000000;
+  	  printf("\nSUMMARY:\nNumber of processes: %d\n Total run-time for %d "
+            "loops with 5 barriers per loop: %fs\n"
+            "The average time per barrier: %fus\n",
+            P, N, total_time/1000000, (double)(total_time/(N*5)));
+  }
   return 0;
 }
